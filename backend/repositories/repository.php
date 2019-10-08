c405df8120339d2ee0eee9c6f54de4f864485329
Jorge Guilherme <jorgekg3@gmail.com>
2019-10-08 12:29:10 -0300

    Update repository.php

--- backend/repositories/repository.php
+++ backend/repositories/repository.php
@@ -1,19 +1,91 @@
 <?php
 
-abstract class Repository {
+abstract class Repository
+{
+  protected $table;
 
-    /** @var PDO */
-    protected $database;
-    protected $table;
+  public function findById($id)
+  {
+    $prepare = Database::getInstance()->prepare("SELECT * FROM {$this->table} WHERE id = ?");
+    $prepare->bindValue(1, $id);
+    $prepare->execute();
+    return $prepare->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-    public function create($data) {
-        $stmt = $this->database->prepare("INSERT INTO " . $this->table . " (endpoint, millis, browser, platform, createAt) values (?, ?, ?, ?, ?)");
-        $stmt->bindValue(1, $data->endpoint);
-        $stmt->bindValue(2, $data->millis);
-        $stmt->bindValue(3, $data->browser);
-        $stmt->bindValue(4, $data->platform);
-        $stmt->bindValue(5, date('Y-m-d h:i:s'));
-        $stmt->execute();
+  public function list($page = 0, $size = 10, $filter = '')
+  {
+    $init = $size * $page;
+    $finish = $size * ($page + 1);
+    $prepare = Database::getInstance()->prepare("SELECT * FROM {$this->table} {$filter} LIMIT {$init}, {$finish}");
+    $prepare->execute();
+    return $prepare->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function insert($data)
+  {
+    $reflection = new ReflectionClass($this->table);
+    $fields = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+    $processFields = array_filter(array_map(function ($field) {
+      if ($field->name != 'id')
+        return $field->name;
+    }, $fields), function ($filter) {
+      return !empty($filter);
+    });
+    $query = "INSERT INTO {$this->table} (";
+    $query .= join(', ', $processFields);
+    $query .= ') VALUES (' . join(', ', array_map(function ($field) {
+      return '?';
+    }, $processFields)) . ')';
+    $prepare = Database::getInstance()->prepare($query);
+    $index = 1;
+    foreach ($processFields as $key) {
+      $prepare->bindValue($index, $data->$key);
+      $index++;
     }
+    $prepare->execute();
+    return $this->findById(Database::getInstance()->lastInsertId());
+  }
 
+  public function delete($id)
+  {
+    $item = $this->findById($id);
+    if (empty($item)) {
+      throw new NotFoundException('This item not exists');
+    }
+    $prepare = Database::getInstance()->prepare("DELETE FROM {$this->table} WHERE id = ?");
+    $prepare->bindValue(1, $id);
+    $prepare->execute();
+  }
+
+  public function update($id, $data)
+  {
+    $item = $this->findById($id);
+    if (empty($item)) {
+      throw new NotFoundException('This item not exists');
+    }
+    $reflection = new ReflectionClass($this->table);
+    $fields = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+
+    $query = "UPDATE {$this->table} SET ";
+    $query .= join(', ', array_filter(array_map(function ($field) use ($data) {
+      $fd = $field->name;
+      if ($field->name != 'id' && !empty($data->$fd))
+        return $field->name . ' = ?';
+    }, $fields), function ($field) {
+      return !empty($field);
+    }));
+    $query .= " WHERE id = ?";
+    $prepare = Database::getInstance()->prepare($query);
+    $index = 1;
+    foreach ($fields as $field) {
+      $field = $field->name;
+      if ($field != 'id' && !empty($data->$field)) {
+        $prepare->bindValue($index, $data->$field);
+        $index++;
+      }
+    }
+    $prepare->bindValue(count($fields), $id);
+    $prepare->execute();
+    return $this->findById($id);
+  }
 }
